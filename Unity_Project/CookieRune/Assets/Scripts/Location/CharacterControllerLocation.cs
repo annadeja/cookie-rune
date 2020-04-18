01edbb771900331d8ddd388f19fe07e021a5394b
Amarbach <33005385+Amarbach@users.noreply.github.com>
2020-04-18 19:25:44 +0200

    Sterowanie w lokacjach
    What it says on the tin.

---
+++ Unity_Project/CookieRune/Assets/Scripts/Location/CharacterControllerLocation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CharacterControllerLocation : MonoBehaviour
+{
+    public Camera mainCam;
+    CharacterController control;
+    [SerializeField] Vector3 disp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        control = GetComponent<CharacterController>();
+        disp = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        disp.x = Input.GetAxis("X") * 7;
+        disp.y -= 10 * Time.deltaTime;
+        if (control.isGrounded)
+        {
+            disp.y = 0;
+            if (Input.GetButton("Jump"))
+            {
+                disp.y = 10;
+            }
+        }
+
+        control.Move(disp * Time.deltaTime);
+
+        if(mainCam != null)
+        {
+            Vector3 camDiff = new Vector3(0, 10, -20);
+            mainCam.transform.position = transform.position + camDiff;
+        }
+    }
+}
---
+++ Unity_Project/CookieRune/Assets/Testing and Debug Assets/CameraTestController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CameraTestController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 dispZ = transform.forward * Input.GetAxis("Z");
+        Vector3 dispX = transform.right * Input.GetAxis("X");
+        Vector3 dispY = transform.up * Input.GetAxis("Y");
+        Vector3 disp = dispZ + dispX + dispY;
+
+        transform.Translate(disp);
+        transform.Rotate(Vector3.up, 3 * Input.GetAxis("Mouse X"), Space.World);
+        transform.Rotate(Vector3.right, -3 * Input.GetAxis("Mouse Y"), Space.World);
+    }
+}
