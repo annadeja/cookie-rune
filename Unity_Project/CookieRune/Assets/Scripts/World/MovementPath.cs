2e83c0f7d31a68bd79f1be8ed51dff6e1a90b1c7
Amarbach <33005385+Amarbach@users.noreply.github.com>
2020-04-25 17:36:02 +0200

    Ścieżki
    Podążanie za sztywnymi ścieżkami na rzecz mapy świata

---
+++ Unity_Project/CookieRune/Assets/Scripts/World/MovementPath.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementPath : MonoBehaviour
+{
+    public int direction = 1;
+    public int curPoint = 0;
+    [SerializeField] public Transform[] waypoints;
+
+    public void nextWaypoint()
+    {
+        if (curPoint >= waypoints.Length - 1 || curPoint <= 0)
+        {
+            direction = -direction;
+        }
+        curPoint += direction;
+    }
+
+    public Transform getCurrentWaypoint() { return waypoints[curPoint]; }
+
+    private void OnDrawGizmos()
+    {
+        if (waypoints != null || waypoints.Length > 2)
+        {
+            for(int i = 0; i < waypoints.Length - 1; i++)
+            {
+                Gizmos.DrawLine(waypoints[i].position, waypoints[i + 1].position);
+            }
+        }
+    }
+}
---
+++ Unity_Project/CookieRune/Assets/Scripts/World/PathFollower.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    [SerializeField] MovementPath path;
+    private Transform moveToWaypoint;
+    Vector3 moveToVec;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (path.waypoints != null)
+        {
+            moveToWaypoint = path.getCurrentWaypoint();
+            transform.position = moveToWaypoint.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if((transform.position - moveToWaypoint.position).magnitude < 0.2f)
+        {
+            Debug.Log("Tu jestem!");
+            path.nextWaypoint();
+            moveToWaypoint = path.getCurrentWaypoint();
+            moveToVec = (moveToWaypoint.position - transform.position);
+        }
+
+        transform.Translate(moveToVec.normalized * moveToVec.magnitude * Time.deltaTime);
+    }
+}
