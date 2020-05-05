092996cde760fbb139c4e38f3a47ec2588f1da81
Amarbach <33005385+Amarbach@users.noreply.github.com>
2020-05-05 19:36:26 +0200

    WALKAAAAAAAA
    O bogowie walka!

---
+++ Unity_Project/CookieRune/Assets/Scripts/Battle/BattleController.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum BattleState { START, PTURN, ETURN, WON, LOST, INACTION, RUN }
+
+public class BattleController : MonoBehaviour
+{
+    public Text enemyName;
+    public Text playerHPtext;
+    public Text dialogue;
+
+    public Slider pHPslider;
+    public Slider eHPslider;
+
+    public BattleState state;
+    public GameObject player;
+    public GameObject enemy;
+
+    public Transform pBattleStation;
+    public Transform eBattleStation;
+
+    Unit playerUnit;
+    Unit enemyUnit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        StartCoroutine(setupBattle());
+    }
+
+    IEnumerator setupBattle()
+    {
+        GameObject pInScene = Instantiate(player, pBattleStation);
+        GameObject eInScene = Instantiate(enemy, eBattleStation);
+
+        playerUnit = pInScene.GetComponent<Unit>();
+        enemyUnit = eInScene.GetComponent<Unit>();
+
+        enemyName.text = enemyUnit.unitName;
+
+        pHPslider.maxValue = playerUnit.maxHP;
+        eHPslider.maxValue = enemyUnit.maxHP;
+
+        updateHP();
+
+        dialogue.text = "Battle starts!";
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PTURN;
+        playerTurn();
+    }
+
+    void updateHP()
+    {
+        pHPslider.value = playerUnit.curHP;
+        eHPslider.value = enemyUnit.curHP;
+        playerHPtext.text = "HP: " + playerUnit.curHP + "/" + playerUnit.maxHP;
+    }
+
+    IEnumerator playerAttack()
+    {
+        state = BattleState.INACTION;
+        //damage enemy
+        bool isDead = enemyUnit.takeDmg(playerUnit.atk);
+
+        updateHP();
+
+        yield return new WaitForSeconds(2f);
+        //say what happened
+        if (isDead)
+        {
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.ETURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    IEnumerator playerFlee()
+    {
+        state = BattleState.INACTION;
+
+        dialogue.text = "You try to flee!";
+
+        yield return new WaitForSeconds(1f);
+
+        float chance = Random.Range(0f, 100f);
+
+        if (chance > 50f)
+        {
+            dialogue.text = "You managed to flee!";
+            state = BattleState.RUN;
+            EndBattle();
+        }
+        else
+        {
+            dialogue.text = "You failed!";
+            yield return new WaitForSeconds(1f);
+            state = BattleState.ETURN;
+            StartCoroutine(EnemyTurn());
+        }
+    }
+
+    void playerTurn()
+    {
+        dialogue.text = "It's your turn.";
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PTURN)
+            return;
+
+        StartCoroutine(playerAttack());
+    }
+
+    public void OnFleeButton()
+    {
+        if (state != BattleState.PTURN)
+            return;
+
+        StartCoroutine(playerFlee());
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        dialogue.text = "Enemy attacks!";
+
+        yield return new WaitForSeconds(.5f);
+
+        bool isDead = playerUnit.takeDmg(enemyUnit.atk);
+        updateHP();
+
+        yield return new WaitForSeconds(.5f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PTURN;
+            playerTurn();
+        }
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            dialogue.text = "You WON!";
+        }
+        else if(state == BattleState.LOST)
+        {
+            dialogue.text = "You Lost!";
+        }
+        else if (state == BattleState.RUN)
+        {
+            dialogue.text = "You fled...";
+        }
+        SceneManager.LoadScene("SugarMine_Scene");
+    }
+}
---
+++ Unity_Project/CookieRune/Assets/Scripts/Unit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    public string unitName;
+    public int level;
+    public int curHP;
+    public int maxHP;
+
+    public int atk;
+    public int def;
+
+    public bool takeDmg(int dmg)
+    {
+        int acDMG = dmg - def;
+        if (acDMG > 0) curHP -= acDMG;
+
+        if (curHP <= 0) return true;
+        return false;
+    }
+}
